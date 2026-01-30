c5826f625f6788047d79461fa366f310fff2fa2c
Amit-Bachar <53488046+MetalBoost5928@users.noreply.github.com>
2026-01-30 15:00:38 +0200

    updated HoodSub and HoodIO

--- src/main/java/frc/robot/subsystems/Shooter/Hood/HoodIO.java
+++ src/main/java/frc/robot/subsystems/Shooter/Hood/HoodIO.java
@@ -2,19 +2,13 @@ package frc.robot.subsystems.Shooter.Hood;
 
 public interface HoodIO {
     public static class HoodIOInputs {
-        public double positionRad = 0.0;
-        public double velocityRadPerSec = 0.0;
-        public double appliedVolts = 0.0;
-        public double currentAmps = 0.0;
+        public double arc = 0.0;
     }
 
     default void updateInputs(HoodIOInputs inputs) {
     }
 
-    default void setTargetPosition(double positionRad) {
-    }
-
-    default void setVoltage(double volts) {
+    default void setTargetArc(double arc) {
     }
 
     default void stop() {
--- src/main/java/frc/robot/subsystems/Shooter/Hood/HoodSUB.java
+++ src/main/java/frc/robot/subsystems/Shooter/Hood/HoodSUB.java
@@ -1,5 +1,69 @@
 package frc.robot.subsystems.Shooter.Hood;
 
-public class HoodSUB {
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.MBSubsystem;
+import frc.robot.subsystems.Shooter.ShooterConstants;
+import frc.robot.util.TunableNumber;
 
+public class HoodSUB extends MBSubsystem {
+
+    private final HoodIO io;
+    private final HoodIO.HoodIOInputs inputs = new HoodIO.HoodIOInputs();
+    private double targetArc = 0.0;
+
+    private final InterpolatingDoubleTreeMap DISTANCE_TO_ARC_MAP = new InterpolatingDoubleTreeMap();
+
+    // Tunables
+    private final TunableNumber testDistance = new TunableNumber("Shooter/Distance", 0.0);
+
+    public HoodSUB(HoodIO io) {
+        super("Hood");
+        this.io = io;
+
+        // Mapping (Distance Meters -> Arc)
+        DISTANCE_TO_ARC_MAP.put(0.0, 0.0);
+        DISTANCE_TO_ARC_MAP.put(0.5, 0.5);
+        DISTANCE_TO_ARC_MAP.put(1.0, 1.0);
+        DISTANCE_TO_ARC_MAP.put(1.5, 1.5);
+        DISTANCE_TO_ARC_MAP.put(2.0, 2.0);
+        DISTANCE_TO_ARC_MAP.put(2.5, 2.5);
+    }
+
+    public void setTargetArc(double targetArc) {
+        this.targetArc = targetArc;
+        io.setTargetArc(targetArc);
+    }
+
+    public void setTargetDistance() {
+        setTargetArc(getArcFromDistance(testDistance.get()));
+    }
+
+    public double getArcFromDistance(double distance) {
+        return DISTANCE_TO_ARC_MAP.get(distance);
+    }
+
+    public double getCurrentArc() {
+        return inputs.arc;
+    }
+
+    public boolean isAtTarget() {
+        return Math.abs(getCurrentArc() - targetArc) < ShooterConstants.kRPMPolerance;
+    }
+
+    public void stop() {
+        io.stop();
+    }
+
+    @Override
+    public void subsystemPeriodic() {
+
+        io.updateInputs(inputs);
+
+        SmartDashboard.putNumber("/Shooter/Arc/Current", inputs.arc);
+
+        SmartDashboard.putNumber("/Shooter/Arc/Target", targetArc);
+
+        SmartDashboard.putBoolean("/Shooter/Arc/At Target", isAtTarget());
+    }
 }
