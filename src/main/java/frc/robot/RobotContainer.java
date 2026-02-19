d3860f64077ca73071bff7d9613aa67846246fba
MetalBoost5928 <53488046+MetalBoost5928@users.noreply.github.com>
2026-02-19 22:56:35 +0200

    סקיצה לקוד , מצריך בדיקה ולחכות שפוז אסטימייטור יסתיים

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,121 +27,136 @@ import frc.robot.subsystems.Shooter.Hood.HoodIOTalonFX;
 import frc.robot.subsystems.Shooter.Hood.HoodSUB;
 
 public class RobotContainer {
-    /* Controllers */
-    private final Joystick driver = new Joystick(0);
-    private final Joystick operator = new Joystick(1);
+        /* Controllers */
+        private final Joystick driver = new Joystick(0);
+        private final Joystick operator = new Joystick(1);
 
-    /* Drive Controls */
-    private final int translationAxis = XboxController.Axis.kLeftY.value;
-    private final int strafeAxis = XboxController.Axis.kLeftX.value;
-    private final int rotationAxis = XboxController.Axis.kRightX.value;
-
-    /* Driver Buttons */
-    private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
-    private final JoystickButton lowerSwerveSpeed = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-    private final JoystickButton higherSwerveSpeed = new JoystickButton(driver,
-            XboxController.Button.kRightBumper.value);
-    private final JoystickButton GoToNearestBranch = new JoystickButton(driver, XboxController.Button.kB.value);
-    private final JoystickButton resetPoseEstimator = new JoystickButton(driver, XboxController.Button.kA.value);
-
-    AutoChooser autoChooser;
-
-    /* Subsystems */
-
-    private HeightSpeedReduction heightSpeedReduction = HeightSpeedReduction.getInstance();
-    private final FlyWheelSub shooter;
-    private final HoodSUB hood;
-    private final Spindexer spindexer = new Spindexer();
-    private final Intake s_intake = new Intake();
-    public final PoseEstimator poseEstimator = new PoseEstimator();
-    public final LimelightSubsystem limelight = new LimelightSubsystem(Constants.VisionConstants.limelight3name);
-    public final LimelightSubsystem limelight2 = new LimelightSubsystem(Constants.VisionConstants.limelight4name);
-    public final Swerve s_Swerve = new Swerve(poseEstimator);
-
-    /// * operation Buttons */
-    private final Trigger shoot = new JoystickButton(operator, XboxController.Button.kX.value);
-    private final JoystickButton spin = new JoystickButton(operator, XboxController.Button.kLeftBumper.value);
-    private final JoystickButton spinAnotherSide = new JoystickButton(operator,
-            XboxController.Button.kRightBumper.value);
-    private final JoystickButton flywheelHoodAutoCommand = new JoystickButton(operator,
-            XboxController.Button.kStart.value);
-    private final Trigger intake = new JoystickButton(operator, XboxController.Button.kB.value);
-    private final Trigger hoodCommand = new JoystickButton(operator, XboxController.Button.kY.value);
-
-    public final GridMap fieldGrid;
-
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    public RobotContainer() {
-        FlyWheelIO shooterIO = RobotBase.isSimulation() ? new FlyWheelSimulation() : new FlyWheelIOTalonFX();
-
-        shooter = new FlyWheelSub(shooterIO);
-
-        HoodIO hoodIO = RobotBase.isSimulation() ? new HoodIOSim() : new HoodIOTalonFX();
-
-        hood = new HoodSUB(hoodIO);
-        fieldGrid = FieldGridLoader.load("FieldGrid.json");
-
-        s_Swerve.setDefaultCommand(
-                new TeleopSwerve(
-                        s_Swerve,
-                        () -> -driver.getRawAxis(translationAxis),
-                        () -> -driver.getRawAxis(strafeAxis),
-                        () -> -driver.getRawAxis(rotationAxis),
-                        () -> true,
-                        heightSpeedReduction.getSpeedSupplier()));
-
-        // Configure the button bindings
-        configureButtonBindings();
-        registerPathPlannerCommands();
-    }
-
-    private void configureButtonBindings() {
-
-        shoot.whileTrue(new Shoot(shooter));
-        spin.whileTrue(new Spin(spindexer, -0.35));
-        spinAnotherSide.whileTrue(new Spin(spindexer, 0.35));
-        intake.whileTrue(new IntakeCommand(s_intake, 0.45));
-        hoodCommand.whileTrue(new HoodCommand(hood));
-        flywheelHoodAutoCommand.whileTrue(new FlywheelHoodIntegrationCommand(shooter, hood));
+        /* Drive Controls */
+        private final int translationAxis = XboxController.Axis.kLeftY.value;
+        private final int strafeAxis = XboxController.Axis.kLeftX.value;
+        private final int rotationAxis = XboxController.Axis.kRightX.value;
 
         /* Driver Buttons */
-        zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
-        lowerSwerveSpeed.whileTrue(
-                new TeleopSwerve(
-                        s_Swerve,
-                        () -> -driver.getRawAxis(translationAxis),
-                        () -> -driver.getRawAxis(strafeAxis),
-                        () -> -driver.getRawAxis(rotationAxis),
-                        () -> true,
-                        () -> 0.4));
+        private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
+        private final JoystickButton lowerSwerveSpeed = new JoystickButton(driver,
+                        XboxController.Button.kLeftBumper.value);
+        private final JoystickButton higherSwerveSpeed = new JoystickButton(driver,
+                        XboxController.Button.kRightBumper.value);
+        private final JoystickButton GoToNearestBranch = new JoystickButton(driver, XboxController.Button.kB.value);
+        private final JoystickButton resetPoseEstimator = new JoystickButton(driver, XboxController.Button.kA.value);
 
-        higherSwerveSpeed.whileTrue(
-                new TeleopSwerve(
-                        s_Swerve,
-                        () -> -driver.getRawAxis(translationAxis),
-                        () -> -driver.getRawAxis(strafeAxis),
-                        () -> -driver.getRawAxis(rotationAxis),
-                        () -> true,
-                        () -> 0.85));
+        AutoChooser autoChooser;
 
-        resetPoseEstimator.onTrue(
-                new InstantCommand(
-                        () -> poseEstimator.sEstimator.resetPosition(
-                                s_Swerve.getGyroYaw(),
-                                s_Swerve.getModulePositions(),
-                                new Pose2d(0, 0, new Rotation2d()))));
+        /* Subsystems */
 
-        GoToNearestBranch.onTrue(
-                PathPlannerUtil.GoToNearestBranch(
-                        poseEstimator.getEstimatedPosition(), Constants.SwerveConstants.constraints));
-    }
+        private HeightSpeedReduction heightSpeedReduction = HeightSpeedReduction.getInstance();
+        private final FlyWheelSub shooter;
+        private final HoodSUB hood;
+        private final Spindexer spindexer = new Spindexer();
+        private final Intake s_intake = new Intake();
+        public final PoseEstimator poseEstimator = new PoseEstimator();
+        public final LimelightSubsystem limelight = new LimelightSubsystem(Constants.VisionConstants.limelight3name);
+        public final LimelightSubsystem limelight2 = new LimelightSubsystem(Constants.VisionConstants.limelight4name);
+        public final Swerve s_Swerve = new Swerve(poseEstimator);
+        public final AlignToPoseSubsystem alignToPose = new AlignToPoseSubsystem();
 
-    public Command getAutonomousCommand() {
-        return autoChooser.getSelectedAuto();
-    }
+        /// * operation Buttons */
+        private final Trigger shoot = new JoystickButton(operator, XboxController.Button.kX.value);
+        private final JoystickButton spin = new JoystickButton(operator, XboxController.Button.kLeftBumper.value);
+        private final JoystickButton spinAnotherSide = new JoystickButton(operator,
+                        XboxController.Button.kRightBumper.value);
+        private final JoystickButton flywheelHoodAutoCommand = new JoystickButton(operator,
+                        XboxController.Button.kStart.value);
+        private final Trigger intake = new JoystickButton(operator, XboxController.Button.kB.value);
+        private final Trigger hoodCommand = new JoystickButton(operator, XboxController.Button.kY.value);
 
-    void registerPathPlannerCommands() {
-    }
+        public final GridMap fieldGrid;
+
+        /**
+         * The container for the robot. Contains subsystems, OI devices, and commands.
+         */
+        public RobotContainer() {
+                FlyWheelIO shooterIO = RobotBase.isSimulation() ? new FlyWheelSimulation() : new FlyWheelIOTalonFX();
+
+                shooter = new FlyWheelSub(shooterIO);
+
+                HoodIO hoodIO = RobotBase.isSimulation() ? new HoodIOSim() : new HoodIOTalonFX();
+
+                hood = new HoodSUB(hoodIO);
+                fieldGrid = FieldGridLoader.load("FieldGrid.json");
+
+                s_Swerve.setDefaultCommand(
+                                new TeleopSwerve(
+                                                s_Swerve,
+                                                () -> -driver.getRawAxis(translationAxis),
+                                                () -> -driver.getRawAxis(strafeAxis),
+                                                () -> -driver.getRawAxis(rotationAxis),
+                                                () -> true,
+                                                heightSpeedReduction.getSpeedSupplier()));
+
+                // Configure the button bindings
+                configureButtonBindings();
+                registerPathPlannerCommands();
+        }
+
+        private void configureButtonBindings() {
+
+                shoot.whileTrue(new Shoot(shooter));
+                spin.whileTrue(new Spin(spindexer, -0.35));
+                spinAnotherSide.whileTrue(new Spin(spindexer, 0.35));
+                intake.whileTrue(new IntakeCommand(s_intake, 0.45));
+                hoodCommand.whileTrue(new HoodCommand(hood));
+                flywheelHoodAutoCommand.whileTrue(new FlywheelHoodIntegrationCommand(shooter, hood));
+
+                /* Driver Buttons */
+                zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
+                lowerSwerveSpeed.whileTrue(
+                                new TeleopSwerve(
+                                                s_Swerve,
+                                                () -> -driver.getRawAxis(translationAxis),
+                                                () -> -driver.getRawAxis(strafeAxis),
+                                                () -> -driver.getRawAxis(rotationAxis),
+                                                () -> true,
+                                                () -> 0.4));
+
+                higherSwerveSpeed.whileTrue(
+                                new TeleopSwerve(
+                                                s_Swerve,
+                                                () -> -driver.getRawAxis(translationAxis),
+                                                () -> -driver.getRawAxis(strafeAxis),
+                                                () -> -driver.getRawAxis(rotationAxis),
+                                                () -> true,
+                                                () -> 0.85));
+
+                resetPoseEstimator.onTrue(
+                                new InstantCommand(
+                                                () -> poseEstimator.sEstimator.resetPosition(
+                                                                s_Swerve.getGyroYaw(),
+                                                                s_Swerve.getModulePositions(),
+                                                                new Pose2d(0, 0, new Rotation2d()))));
+
+                GoToNearestBranch.onTrue(
+                                PathPlannerUtil.GoToNearestBranch(
+                                                poseEstimator.getEstimatedPosition(),
+                                                Constants.SwerveConstants.constraints));
+
+                // Shoot While Moving
+                new JoystickButton(driver, XboxController.Button.kX.value).whileTrue(
+                                new ShootWhileMoving(
+                                                s_Swerve,
+                                                poseEstimator,
+                                                hood,
+                                                shooter,
+                                                spindexer,
+                                                alignToPose,
+                                                () -> -driver.getRawAxis(translationAxis),
+                                                () -> -driver.getRawAxis(strafeAxis)));
+        }
+
+        public Command getAutonomousCommand() {
+                return autoChooser.getSelectedAuto();
+        }
+
+        void registerPathPlannerCommands() {
+        }
 }
---
+++ src/main/java/frc/robot/commands/ShootWhileMoving.java
@@ -0,0 +1,142 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.PoseEstimator;
+import frc.robot.subsystems.Shooter.FlyWheel.FlyWheelSub;
+import frc.robot.subsystems.Shooter.Hood.HoodSUB;
+import frc.robot.subsystems.Spindexer;
+import frc.robot.subsystems.AlignToPoseSubsystem;
+import frc.robot.subsystems.Swerve;
+import frc.robot.util.FlightTimeTable;
+import frc.robot.util.ShotPrediction;
+import java.util.Optional;
+import java.util.function.DoubleSupplier;
+
+public class ShootWhileMoving extends Command {
+    private final Swerve swerve;
+    private final PoseEstimator poseEstimator;
+    private final HoodSUB hood;
+    private final FlyWheelSub flywheel;
+    private final Spindexer spindexer;
+    private final AlignToPoseSubsystem alignSubsystem;
+    private final DoubleSupplier translationXSupplier;
+    private final DoubleSupplier translationYSupplier;
+
+    // Constants for this command
+    private static final double INDEX_SPEED = 0.35; // Matches Spin command usage
+
+    public ShootWhileMoving(
+            Swerve swerve,
+            PoseEstimator poseEstimator,
+            HoodSUB hood,
+            FlyWheelSub flywheel,
+            Spindexer spindexer,
+            AlignToPoseSubsystem alignSubsystem,
+            DoubleSupplier translationXSupplier,
+            DoubleSupplier translationYSupplier) {
+
+        this.swerve = swerve;
+        this.poseEstimator = poseEstimator;
+        this.hood = hood;
+        this.flywheel = flywheel;
+        this.spindexer = spindexer;
+        this.alignSubsystem = alignSubsystem;
+        this.translationXSupplier = translationXSupplier;
+        this.translationYSupplier = translationYSupplier;
+
+        addRequirements(swerve, hood, flywheel, spindexer, alignSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        // 1. Get Current Pose
+        Pose2d currentPose = poseEstimator.getEstimatedPosition();
+
+        // 2. Determine Hub Position based on Alliance
+        Translation2d hubPosition = Constants.FieldConstants.HUB_CENTER_BLUE;
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+        if (alliance.isPresent() && alliance.get() == Alliance.Red) {
+            hubPosition = Constants.FieldConstants.HUB_CENTER_RED;
+        }
+
+        // 3. Calculate Distance to Hub (for Setpoints) & Set Subsystems
+        double distanceToHub = currentPose.getTranslation().getDistance(hubPosition);
+
+        flywheel.setTargetRPM(flywheel.getRPMFromDistance(distanceToHub));
+        hood.setTargetArc(hood.getArcFromDistance(distanceToHub));
+
+        // 4. Get Flight Time
+        double flightTime = FlightTimeTable.get(distanceToHub);
+
+        // 5. Get Field Relative Speeds
+        // Swerve gives RobotRelative speeds. We need to convert to FieldRelative
+        // manually or use helper.
+        ChassisSpeeds robotRelativeSpeeds = swerve.getRobotRelativeSpeeds();
+
+        // Convert RobotVelocity (vx, vy) to FieldVelocity by rotating by robot heading
+        Translation2d robotLinearVelocity = new Translation2d(
+                robotRelativeSpeeds.vxMetersPerSecond,
+                robotRelativeSpeeds.vyMetersPerSecond);
+        Translation2d fieldLinearVelocity = robotLinearVelocity.rotateBy(currentPose.getRotation());
+
+        ChassisSpeeds fieldRelativeSpeeds = new ChassisSpeeds(
+                fieldLinearVelocity.getX(),
+                fieldLinearVelocity.getY(),
+                robotRelativeSpeeds.omegaRadiansPerSecond);
+
+        // Deadband speeds to reduce noise when standing still (Requested by user)
+        if (Math.abs(fieldRelativeSpeeds.vxMetersPerSecond) < 0.1)
+            fieldRelativeSpeeds.vxMetersPerSecond = 0;
+        if (Math.abs(fieldRelativeSpeeds.vyMetersPerSecond) < 0.1)
+            fieldRelativeSpeeds.vyMetersPerSecond = 0;
+
+        // 6. Predict Future Position
+        // futurePos = currentPos + (vx, vy) * flightTime
+        Translation2d futurePos = ShotPrediction.predictFuturePosition(currentPose, fieldRelativeSpeeds, flightTime);
+
+        // 7. Calculate Rotation Output using AlignToPoseSubsystem
+        // We act "as if" we are at the future position but with current rotation,
+        // asking the subsystem to face the hub from that position.
+        Pose2d futurePoseForCalc = new Pose2d(futurePos, currentPose.getRotation());
+        double rotationOutput = alignSubsystem.calculateRotationOutput(futurePoseForCalc, hubPosition);
+
+        // Driver control for translation
+        double xSpeed = translationXSupplier.getAsDouble();
+        double ySpeed = translationYSupplier.getAsDouble();
+
+        // Drive field relative
+        swerve.drive(
+                new Translation2d(xSpeed, ySpeed),
+                rotationOutput,
+                true, // fieldRelative
+                false // isOpenLoop
+        );
+
+        // 8. Gating & Shooting
+        boolean hoodReady = hood.isAtTarget();
+        boolean flywheelReady = flywheel.isAtTarget();
+        boolean aligned = alignSubsystem.atSetpoint();
+
+        if (hoodReady && flywheelReady && aligned) {
+            spindexer.SetMotor(INDEX_SPEED); // Feed
+        } else {
+            spindexer.SetMotor(0);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        swerve.drive(new Translation2d(0, 0), 0, true, false);
+        spindexer.SetMotor(0);
+        hood.stop();
+        flywheel.stop();
+    }
+}
