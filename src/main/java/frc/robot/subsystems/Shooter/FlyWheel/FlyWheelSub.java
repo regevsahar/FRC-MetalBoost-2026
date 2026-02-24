bfc7a25dc303468f94c87976515e8233673428aa
EyalAvru <eyalavru@gmail.com>
2026-02-24 21:27:39 +0200

    pulled , need to test

--- src/main/java/frc/robot/subsystems/Shooter/FlyWheel/FlyWheelSub.java
+++ src/main/java/frc/robot/subsystems/Shooter/FlyWheel/FlyWheelSub.java
@@ -4,6 +4,7 @@ import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.lib.util.TunableNumber;
 import frc.robot.subsystems.MBSubsystem;
 import frc.robot.subsystems.Shooter.ShooterConstants;
 
--- src/main/java/frc/robot/subsystems/Shooter/Hood/HoodSUB.java
+++ src/main/java/frc/robot/subsystems/Shooter/Hood/HoodSUB.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.subsystems.MBSubsystem;
+import frc.lib.util.TunableNumber;
 import frc.robot.subsystems.Shooter.ShooterConstants;
 
 public class HoodSUB extends MBSubsystem {
